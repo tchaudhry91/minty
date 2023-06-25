c59a05226e794e05aa9adeecf9c709cfd6446f1c
Tanmay Chaudhry <tanmay.chaudhry@gmail.com>
2023-06-25 14:31:30 +0530

    update lexer

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 const tokens = @import("./tokens.zig");
 const Types = tokens.TokenType;
 
@@ -19,6 +20,7 @@ const Lexer = struct {
 
     pub fn nextToken(self: *Lexer) tokens.Token {
         defer self.readChar();
+        self.skipWhitespace();
         var token_type: Types = switch (self.ch) {
             '=' => Types.ASSIGN,
             ';' => Types.SEMICOLON,
@@ -29,10 +31,54 @@ const Lexer = struct {
             '{' => Types.LBRACE,
             '}' => Types.RBRACE,
             0 => Types.EOF,
-            else => Types.ILLEGAL,
+            else => {
+                if (self.isAllowedLetter()) {
+                    const literal = self.readIdentifier();
+                    return tokens.Token.New(tokens.lookupIdentifierType(literal), literal);
+                }
+                if (self.isNumber()) {
+                    const literal = self.readNumber();
+                    return tokens.Token.New(Types.INT, literal);
+                } else {
+                    std.debug.print("illegal character: {}\n", .{self.ch});
+                    return tokens.Token.New(Types.ILLEGAL, &[_]u8{self.ch});
+                }
+            },
         };
-        const token = tokens.Token.New(token_type, self.ch);
+        const token = tokens.Token.New(token_type, &[_]u8{self.ch});
         return token;
+    }
+
+    fn readIdentifier(self: *Lexer) []const u8 {
+        const position = self.position;
+        while (self.isAllowedLetter() and self.ch != 0) {
+            self.readChar();
+        }
+        return self.input[position..self.position];
+    }
+
+    fn isAllowedLetter(self: *Lexer) bool {
+        return ('a' <= self.ch and self.ch <= 'z') or
+            ('A' <= self.ch and self.ch <= 'Z') or
+            self.ch == '_';
+    }
+
+    fn isNumber(self: *Lexer) bool {
+        return '0' <= self.ch and self.ch <= '9';
+    }
+
+    fn readNumber(self: *Lexer) []const u8 {
+        const position = self.position;
+        while (self.isNumber()) {
+            self.readChar();
+        }
+        return self.input[position..self.position];
+    }
+
+    fn skipWhitespace(self: *Lexer) void {
+        while (self.ch == ' ' or self.ch == '\t' or self.ch == '\n' or self.ch == '\r') {
+            self.readChar();
+        }
     }
 
     pub fn init(input: []const u8) Lexer {
@@ -44,8 +90,8 @@ const Lexer = struct {
     }
 };
 
-const std = @import("std");
-const expect = std.testing.expect;
+// Tests
+
 test "token_parse" {
     const input: []const u8 = "=+(){},;";
     const expected_tokens = [_]Types{
@@ -75,7 +121,101 @@ test "token_parse" {
     var tok: tokens.Token = undefined;
     for (expected_tokens, 0..) |expected_token, i| {
         tok = lex.nextToken();
-        try expect(tok.type == expected_token);
-        try expect(tok.literal.?[0] == literals[i]);
+        try std.testing.expect(tok.type == expected_token);
+        try std.testing.expect(tok.literal.?[0] == literals[i]);
+    }
+}
+
+test "src_parse" {
+    const input: []const u8 =
+        \\ let five = 5;
+        \\ let ten = 10;
+        \\ let add = fn(x, y) {
+        \\     x + y;
+        \\ };
+        \\ let result = add(five, ten);
+    ;
+
+    const expected_tokens = [_]Types{
+        Types.LET,
+        Types.IDENT,
+        Types.ASSIGN,
+        Types.INT,
+        Types.LET,
+        Types.IDENT,
+        Types.ASSIGN,
+        Types.INT,
+        Types.LET,
+        Types.IDENT,
+        Types.ASSIGN,
+        Types.FUNCTION,
+        Types.LPAREN,
+        Types.IDENT,
+        Types.COMMA,
+        Types.IDENT,
+        Types.RPAREN,
+        Types.LBRACE,
+        Types.IDENT,
+        Types.PLUS,
+        Types.IDENT,
+        Types.SEMICOLON,
+        Types.RBRACE,
+        Types.SEMICOLON,
+        Types.LET,
+        Types.IDENT,
+        Types.ASSIGN,
+        Types.IDENT,
+        Types.LPAREN,
+        Types.IDENT,
+        Types.COMMA,
+        Types.IDENT,
+        Types.RPAREN,
+        Types.SEMICOLON,
+        Types.EOF,
+    };
+    const literals = [_][]const u8{
+        "let",
+        "five",
+        "=",
+        "5",
+        "let",
+        "ten",
+        "=",
+        "10",
+        "let",
+        "add",
+        "=",
+        "fn",
+        "(",
+        "x",
+        ",",
+        "y",
+        ")",
+        "{",
+        "x",
+        "+",
+        "y",
+        ";",
+        "}",
+        ";",
+        "let",
+        "result",
+        "=",
+        "add",
+        "(",
+        "five",
+        ",",
+        "ten",
+        ")",
+        ";",
+        "0",
+    };
+    var lex = Lexer.init(input);
+
+    var tok: tokens.Token = undefined;
+    for (expected_tokens, 0..) |expected_token, i| {
+        tok = lex.nextToken();
+        try std.testing.expectEqual(expected_token, tok.type);
+        try std.testing.expectEqualSlices(u8, tok.literal.?, literals[i]);
     }
 }
--- src/tokens.zig
+++ src/tokens.zig
@@ -1,3 +1,4 @@
+const std = @import("std");
 pub const TokenType = enum {
     ILLEGAL,
     EOF,
@@ -27,10 +28,22 @@ pub const Token = struct {
     type: TokenType,
     literal: ?[]const u8,
 
-    pub fn New(token_type: TokenType, ch: u8) Token {
+    pub fn New(token_type: TokenType, literal: []const u8) Token {
         return Token{
             .type = token_type,
-            .literal = &[_]u8{ch},
+            .literal = literal,
         };
     }
 };
+
+pub fn lookupIdentifierType(literal: []const u8) TokenType {
+    // Since there is no switch on strings, we have to do this manually.
+    // to-do: Look for a better method later.
+    if (std.mem.eql(u8, literal, "fn")) {
+        return TokenType.FUNCTION;
+    } else if (std.mem.eql(u8, literal, "let")) {
+        return TokenType.LET;
+    } else {
+        return TokenType.IDENT;
+    }
+}
